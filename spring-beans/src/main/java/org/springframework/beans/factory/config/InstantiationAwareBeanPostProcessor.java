9c8b584cda013ddea9705015f1e0e6f8d8cef77e
renlw123 <1981890456@qq.com>
2026-04-28 15:47:08 +0800

    [bean生命周期的第三个扩展点InstantiationAwareBeanPostProcessor]

--- spring-beans/src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessor.java
@@ -74,22 +74,37 @@ public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
 	}
 
 	/**
-	 * Perform operations after the bean has been instantiated, via a constructor or factory method,
-	 * but before Spring property population (from explicit properties or autowiring) occurs.
-	 * <p>This is the ideal callback for performing custom field injection on the given bean
-	 * instance, right before Spring's autowiring kicks in.
-	 * <p>The default implementation returns {@code true}.
-	 * @param bean the bean instance created, with properties not having been set yet
-	 * @param beanName the name of the bean
-	 * @return {@code true} if properties should be set on the bean; {@code false}
-	 * if property population should be skipped. Normal implementations should return {@code true}.
-	 * Returning {@code false} will also prevent any subsequent InstantiationAwareBeanPostProcessor
-	 * instances being invoked on this bean instance.
-	 * @throws org.springframework.beans.BeansException in case of errors
-	 * @see #postProcessBeforeInstantiation
+	 * 在 bean 实例化完成之后、Spring 属性填充之前执行的操作。
+	 *
+	 * <p><b>调用时机：</b>
+	 * 构造函数执行完毕，但依赖注入（@Autowired、setter方法等）尚未开始
+	 *
+	 * <p><b>返回值含义：</b>
+	 * <ul>
+	 *   <li>{@code true}（默认）：继续进行属性填充（依赖注入）</li>
+	 *   <li>{@code false}：<b>跳过所有属性填充</b>，并且后续的 InstantiationAwareBeanPostProcessor
+	 *       也不会再被调用（包括 postProcessProperties）</li>
+	 * </ul>
+	 *
+	 * <p><b>典型应用场景：</b>
+	 * <ul>
+	 *   <li>对刚实例化的 bean 进行自定义字段注入（在 Spring 注入之前）</li>
+	 *   <li>根据某些条件决定是否让 Spring 进行依赖注入</li>
+	 *   <li>对 bean 实例进行额外的初始化操作（此时属性都还是默认值）</li>
+	 * </ul>
+	 *
+	 * <p><b>注意事项：</b>
+	 * <ul>
+	 *   <li>此时 bean 已经通过构造函数创建出来，但所有属性都是默认值（null、0、false等）</li>
+	 *   <li>如果返回 false，不仅会跳过属性填充，还会跳过该接口后续的 postProcessProperties 方法</li>
+	 * </ul>
+	 *
+	 * @param bean 已创建的 bean 实例，属性尚未被设置
+	 * @param beanName bean 的名称
+	 * @return true：继续属性填充；false：跳过属性填充并阻止后续处理器调用
 	 */
 	default boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-		return true;
+		return true;  // 默认实现：继续执行
 	}
 
 	/**
